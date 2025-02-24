22da106521675be3a47ac2a6d475a9677249a3df
anamariacaballero <anamariacaballero@vitaly.es>
2025-02-24 18:56:18 +0100

    04-prototype.ts

--- 01-creacionales/04-prototype.ts
+++ 01-creacionales/04-prototype.ts
@@ -9,3 +9,36 @@
  * 
  * https://refactoring.guru/es/design-patterns/prototype
  */
+
+class Documento {
+    public title: string;
+    private content: string;
+    public author: string;
+
+    constructor(title: string, content: string, author: string) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    displayInfo() {
+        console.log(`Title: ${this.title}`);
+        console.log(`Content: ${this.content}`);
+        console.log(`Author: ${this.author}`);
+    }
+}
+
+
+function main() {
+    const doc1 = new Documento('Documento 1', 'Content 1', 'Author 1');
+
+    console.log({doc1});
+    doc1.displayInfo();
+
+    const doc2 = {...doc1}; // Se pierde su ADN
+    doc2.title = 'Nuevo titulo';
+
+
+}
+
+main();
